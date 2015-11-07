b76a57bb5238740ec0722e932bd978340ee2e465
Pavel Savinov <contact@getjump.me>
2015-11-07 19:06:43 +1000

    Photo size normalno

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,8 +3,8 @@ require 'carrierwave/orm/activerecord'
 class Photo < ActiveRecord::Base
   has_attached_file :photo, :default_url => "https://pp.vk.me/c627626/v627626270/20a3a/Vn0ltSG-bFE.jpg", styles: {
     thumb: '150x150>',
-    square: '300x300#',
-    medium: '400x400>'
+    square: '360x360#',
+    medium: '450x450>'
   }
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
