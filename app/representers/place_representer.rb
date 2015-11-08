359c2551ca3991d0bf8e5026fd44f8f219580472
Pavel Savinov <contact@getjump.me>
2015-11-08 12:41:38 +1000

    Id to place representer

--- app/representers/place_representer.rb
+++ app/representers/place_representer.rb
@@ -4,6 +4,7 @@ require 'roar/json'
 class PlaceRepresenter < Roar::Decorator
   include Roar::JSON
 
+  property :id
   property :name
   property :address
   property :location, extend: PointRepresenter
